7f20e9d5072ef5cf73f9ab0d0e2eeabc77bbeb2e
saman-zdf <samanfyz68@gmail.com>
2022-01-28 07:18:19 +0800

    added env file

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
 // /in ES6 module you need to add .js extention for importing
+// Middleware
 import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 app.get('/', (req, res) => {
+  throw new Error('error');
   res.status(200).send('hello world');
 });
-// Middleware
+
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log('server running on port 5000');
+  console.log(`server runngin on port ${PORT}`);
 });
