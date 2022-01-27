766c553552fe21e9f84b1aaf9c0fa4136708c0ee
saman-zdf <samanfyz68@gmail.com>
2022-01-28 07:07:13 +0800

    created notFound function and used it as a middleware

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 const app = express();
 
+// /in ES6 module you need to add .js extention for importing
+import notFoundMiddleware from './middleware/not-found.js';
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.status(200).send('hello world');
 });
+// Middleware
+app.use(notFoundMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
