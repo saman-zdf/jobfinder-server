308353b445156401ddedfac7de476442bac87591
saman-zdf <samanfyz68@gmail.com>
2022-01-28 08:12:38 +0800

    added basic auth controller

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
-import connectDB from './databse/connectDB.js';
+import connectDB from './DB/connectDB.js';
 
-// /in ES6 module you need to add .js extention for importing
+// /in ES6 module you need to add .js extension for importing
 // Middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('hello world');
 });
+// auth router
+import authRoutes from './routes/authRoutes.js';
+app.use('/user', authRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -24,7 +28,7 @@ const start = async () => {
     await connectDB(process.env.MONGO_URI);
     console.log('database connected');
     app.listen(PORT, () => {
-      console.log(`server runngin on port ${PORT}`);
+      console.log(`server running on port ${PORT}`);
     });
   } catch (error) {
     console.log(error);
