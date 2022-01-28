96ac9c203ce3f31d0ee1fe1150991da3f46459b1
saman-zdf <samanfyz68@gmail.com>
2022-01-28 10:34:52 +0800

    email validation has been added using validator package

--- model/user.js
+++ model/user.js
@@ -1,27 +1,45 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+// Schema always start with "new" key word to create
+const UserSchema = new Schema({
   name: {
     type: String,
     required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
   },
-  lastName: {
-    type: String,
-    required: [true, 'Please provide last name'],
-  },
+
   email: {
     type: String,
     required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email',
+    },
+    // this is not a validator, but it can ensure the user does not register with same email twice
+    unique: true,
   },
   password: {
     type: String,
     required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: 'lastName',
   },
   location: {
     type: String,
+    trim: true,
+    maxlength: 20,
+    default: 'my city',
   },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', UserSchema);
 export default User;
