a15a44bd94969dfc7d5292c6c71b891ae17b7eaf
saman-zdf <samanfyz68@gmail.com>
2022-01-28 11:15:39 +0800

    created default error object in error handler, added properties for status code and error msg

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,24 @@
 import { StatusCodes } from 'http-status-codes';
 const errorHandlerMiddleware = (err, req, res, next) => {
+  // creating a default error object
   const defaultError = {
+    // adding statusCode property and assign it to the status codes package
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: 'Something went wrong, try again later!',
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+  // check if the error name is equal to ValidationError
+  if (err.name === 'ValidationError') {
+    // assign a bad request to the default error status code
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    // if there is more than one error message we can use Object.values() pass the error object as argument, map through it and get the messages and then join them together
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+  }
+  // res.status(defaultError.statusCode).json({ msg: err });
+  // send response
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
