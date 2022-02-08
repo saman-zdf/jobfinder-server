c5eb1432c47239f24b64c115d9032b620084e920
saman-zdf <samanfyz68@gmail.com>
2022-02-09 07:03:14 +0800

    sort functionality has been added

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -27,6 +27,18 @@ const getAllJobs = async (req, res) => {
   }
   // no await
   let result = Job.find(queryObject);
+  if (sort === 'latest') {
+    result = result.sort('-createdAt');
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt');
+  }
+  if (sort === 'a-z') {
+    result = result.sort('position');
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-position');
+  }
 
   // chain sort condition
   const jobs = await result;
