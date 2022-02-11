fed50bd034f5101f7046a61a4a13017ebd02495e
saman-zdf <samanfyz68@gmail.com>
2022-02-11 11:02:49 +0800

    added pagination with skip and limit from mongoose

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -40,12 +40,24 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position');
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  // skip() from mongoose will skip the number of jobs that has been specified and limit() from mongoose will determine how many item to show, basically set limitation of item to respond for the request
+  result = result.skip(skip).limit(limit);
+
   // chain sort condition
   const jobs = await result;
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPage: 1 });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPage = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({
+    jobs,
+    totalJobs,
+    numOfPage,
+  });
 };
 
 // created job
